17aaf3892db18f00de557826575c58e81713e87c
smithwebtek <brad@smithwebtek.com>
2018-01-04 17:52:27 -0500

    commenting out combineReducers code, to render burger for end of video 292

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+// import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
-
+// import orderReducer from './store/reducers/order';
 
 // const rootReducer = combineReducers({
-//   brg: burgerBuilder
+//   brg: burgerBuilderReducer,
+//   ord: orderReducer
 // });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
