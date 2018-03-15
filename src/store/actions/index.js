cd1fca27facfcc2d64155c60c7aa3fea8aabbfc1
smithwebtek <brad@smithwebtek.com>
2018-03-15 06:13:41 -0400

    Typo in comment

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,24 +1,24 @@
 export {
-  addIngredient,
-  removeIngredient,
-  initIngredients
-  // fetchIngredientsFailed
+	addIngredient,
+	removeIngredient,
+	initIngredients
+	// fetchIngredientsFailed
 } from './burgerBuilder';
 
 export {
-  purchaseBurger,
-  // purchaseBurgerStart,
-  // purchaseBurgerSuccess,
-  // purchaseBurgerFail,
-  purchaseInit,
-  // fetchOrdersStart,
-  // fetchOrdersSuccess,
-  // fetchOrdersFail,
-  fetchOrders
+	purchaseBurger,
+	// purchaseBurgerStart,
+	// purchaseBurgerSuccess,
+	// purchaseBurgerFail,
+	purchaseInit,
+	// fetchOrdersStart,
+	// fetchOrdersSuccess,
+	// fetchOrdersFail,
+	fetchOrders
 } from './order';
 
 
 // the only actionCreators needed in this file, are ones that are called from containers or components
 // who is calling who? 
 // if an actionCreator is only called by another actionCreator, it does not need to be in this file
-// futher, it may be a security risk if someone could dispatch an actionCreator improperly
+// further, it may be a security risk if someone could dispatch an actionCreator improperly
