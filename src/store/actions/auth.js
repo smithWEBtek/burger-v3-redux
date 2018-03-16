bc35b8567eae84199f0fa7c5fc7efc016c1630c0
smithwebtek <brad@smithwebtek.com>
2018-03-15 23:47:24 -0400

    Update auth

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios'
 
 export const authStart = () => {
 	return {
@@ -20,9 +21,27 @@ export const authFail = (error) => {
 	}
 }
 
+
 export const auth = (email, password) => {
 	return dispatch => {
 		dispatch(authStart());
+
+		const authData = {
+			email: email,
+			password: password,
+			returnSecureToken: true
+		}
 		console.log('[auth.js][auth][email, password]: ', email, password);
+		axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyA2zOFXIivxut360JgLehkoBf8PinQCjtE', authData)
+			.then(response => {
+				console.log((response));
+				dispatch(authSuccess(response.data))
+			})
+			.catch(err => {
+				console.log(err);
+				dispatch(authFail(err))
+			})
 	}
 }
+
+// apiKey: "AIzaSyA2zOFXIivxut360JgLehkoBf8PinQCjtE"
